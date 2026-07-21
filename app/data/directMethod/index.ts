535df74043be87a763565aeb7931fc543b516039
Lasha Khvedelidze <lasha.khvedelidze@space.ge>
2026-07-21 18:09:51 +0400

    Added Direct Method Unit 2

--- app/data/directMethod/index.ts
+++ app/data/directMethod/index.ts
@@ -3,11 +3,23 @@ import { questions as questions1 } from "./unit/1/questions";
 import { grammar as grammar1 } from "./unit/1/grammar";
 import { exercises as exercises1 } from "./unit/1/exercises";
 
+import { vocabulary as vocabulary2 } from "./unit/2/vocabulary";
+import { questions as questions2 } from "./unit/2/questions";
+import { grammar as grammar2 } from "./unit/2/grammar";
+import { exercises as exercises2 } from "./unit/2/exercises";
+
 export const directMethodData = {
   1: {
     vocabulary: vocabulary1,
     questions: questions1,
     grammar: grammar1,
     exercises: exercises1,
+  },
+
+  2: {
+    vocabulary: vocabulary2,
+    questions: questions2,
+    grammar: grammar2,
+    exercises: exercises2,
   },
 };
